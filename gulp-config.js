bd1ee2b2464e87c305f98e7e80e28f87086d37d2
nponnaganti <nag.mrtn41@gmail.com>
2019-07-13 13:27:08 +0000

    02 Commit CHanged Default settings

--- gulp-config.js
+++ gulp-config.js
@@ -1,10 +1,10 @@
 module.exports = function () {
-  var instanceRoot = "C:\\inetpub\\wwwroot\\habitat.dev.local";
+  var instanceRoot = "C:\\inetpub\\wwwroot\\sc910.sc";
   var config = {
     websiteRoot: instanceRoot + "\\",
     sitecoreLibraries: instanceRoot + "\\bin",
     licensePath: instanceRoot + "\\App_Data\\license.xml",
-    packageXmlBasePath: ".\\src\\Project\\Habitat\\code\\App_Data\\packages\\habitat.xml",
+    packageXmlBasePath: ".\\src\\Project\\site910\\Habitat\\code\\App_Data\\packages\\habitat.xml",
     packagePath: instanceRoot + "\\App_Data\\packages",
     solutionName: "Habitat",
     buildConfiguration: "Debug",
