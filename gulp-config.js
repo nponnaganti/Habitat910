06aed0d48ea6e10ff91a3a1c1c388ab9ab34d83f
nponnaganti <nag.mrtn41@gmail.com>
2019-08-02 04:13:11 +0000

    Config changes for gulp deafult

--- gulp-config.js
+++ gulp-config.js
@@ -1,14 +1,14 @@
 module.exports = function () {
   var instanceRoot = "C:\\inetpub\\wwwroot\\sc910.sc";
-  var config = {
+  var config = { 
     websiteRoot: instanceRoot + "\\",
     sitecoreLibraries: instanceRoot + "\\bin",
     licensePath: instanceRoot + "\\App_Data\\license.xml",
-    packageXmlBasePath: ".\\src\\Project\\site910\\Habitat\\code\\App_Data\\packages\\habitat.xml",
+    packageXmlBasePath: ".\\src\\Project\\site902\\Habitat\\code\\App_Data\\packages\\habitat.xml",
     packagePath: instanceRoot + "\\App_Data\\packages",
     solutionName: "Habitat",
     buildConfiguration: "Debug",
-    buildToolsVersion: 15.0,
+    buildToolsVersion: 15.0, // 14.0 for vs2015
     buildMaxCpuCount: 0,
     buildVerbosity: "minimal",
     buildPlatform: "Any CPU",
